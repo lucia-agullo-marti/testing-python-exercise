0b73e901d204b28c64f045d4c5079948684d1515
Lucía Agulló Martí <luciaagullo@outlook.com>
2026-01-17 13:25:49 +0100

    Fix default temperature parameters to be floats

--- diffusion2d.py
+++ diffusion2d.py
@@ -49,7 +49,7 @@ class SolveDiffusion2D:
         self.nx = int(w / dx)
         self.ny = int(h / dy)
 
-    def initialize_physical_parameters(self, d=4., T_cold=300, T_hot=700):
+    def initialize_physical_parameters(self, d=4., T_cold=300., T_hot=700.):
         assert isinstance(d,float), 'd must be a float'
         assert isinstance(T_cold,float), 'T_cold must be a float'
         assert isinstance(T_hot,float), 'T_hot must be a float' 
